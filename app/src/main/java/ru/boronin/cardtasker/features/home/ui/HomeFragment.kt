7b2ed1984cbd63989c484511cc194ce5ce0d29fe
sboronin <Sergey.Boronin@bia-tech.ru>
2020-02-21 12:03:19 +0300

    add customtablayout

--- app/src/main/java/ru/boronin/cardtasker/features/home/ui/HomeFragment.kt
+++ app/src/main/java/ru/boronin/cardtasker/features/home/ui/HomeFragment.kt
@@ -9,8 +9,11 @@ import ru.boronin.cardtasker.features.details.ui.DetailsFragment
 import ru.boronin.cardtasker.features.home.di.HomeComponent
 import ru.boronin.cardtasker.features.main.di.activity.ActivityComponent
 import ru.boronin.cardtasker.features.tasks.ui.TasksFragment
+import ru.boronin.common.extension.core.findColor
 import ru.boronin.common.extension.widget.fadeOutIn
+import ru.boronin.common.utils.DEFAULT_COLOR
 import javax.inject.Inject
+
 
 /**
  * Created by Sergey Boronin on 14.01.2020.
@@ -51,9 +54,37 @@ class HomeFragment : BaseView<HomeView, HomePresenter, HomeComponent>(), HomeVie
     }
 
     private fun initBottomBar() {
-        bottomNavigation?.setOnNavigationItemSelectedListener { item ->
-            switchTab(item.itemId)
-            true
+//        bottomNavigation?.setOnNavigationItemSelectedListener { item ->
+//            switchTab(item.itemId)
+//            true
+//        }
+        customTabLayout.addTabs(
+            listOf(
+                TabData(
+                    R.drawable.ic_playlist_add_check_black_24dp,
+                    getString(R.string.bottomBar_item1)
+                ),
+                TabData(
+                    R.drawable.ic_alarm_black_24dp,
+                    getString(R.string.bottomBar_item2)
+                ),
+                TabData(
+                    R.drawable.ic_notifications_none_black_24dp,
+                    getString(R.string.bottomBar_item3)
+                ),
+                TabData(
+                    R.drawable.ic_settings_black_24dp,
+                    getString(R.string.bottomBar_item4)
+                )
+            )
+        )
+        customTabLayout.setActiveIconColor(context.findColor(R.color.colorAccent) ?: DEFAULT_COLOR)
+        customTabLayout.setActiveTextColor(context.findColor(R.color.colorAccent) ?: DEFAULT_COLOR)
+        customTabLayout.activateTab(0)
+        customTabLayout.setOnTabClickListener {index ->
+            if (index != viewPager2.currentItem) {
+                viewPager2.fadeOutIn { viewPager2.setCurrentItem(index, false) }
+            }
         }
     }
 
